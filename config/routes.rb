62ec5349ebeda9805110138f54de384bc98aecd9
jwolfe890 <jwolfe890@gmail.com>
2017-01-07 15:20:50 -0500

    all

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'products#add'
+
+  get 'products#show' => "products#add"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
